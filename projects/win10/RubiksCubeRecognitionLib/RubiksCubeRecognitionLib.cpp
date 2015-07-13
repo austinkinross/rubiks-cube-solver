65db43f9d44a5e52f14b881b77110191b401714b
Austin Kinross <austinkinross@outlook.com>
2015-07-13 13:25:19 -0700

    Add Win10 'RubiksCubeRecognitionLib'

---
+++ projects/win10/RubiksCubeRecognitionLib/RubiksCubeRecognitionLib.cpp
@@ -0,0 +1,10 @@
+﻿
+#include "RubiksCubeRecognitionLib.h"
+
+#include "Cube/Cube.h"
+#include "Cube/CubeCommandList.h"
+
+void colorCubeFace(Cube* cube)
+{
+
+}
---
+++ projects/win10/RubiksCubeRecognitionLib/RubiksCubeRecognitionLib.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+class Cube;
+
+void colorCubeFace(Cube* cube)
+{
+
+}
