15563235684e71872e4fd3c6621f6b6ff1d33049
Austin Kinross <austinkinross@outlook.com>
2015-07-15 09:55:22 -0700

    Cleanup RendererGL_WindowsStore

--- src/Renderer/GL/RendererGL_WindowsStore.cpp
+++ src/Renderer/GL/RendererGL_WindowsStore.cpp
@@ -25,153 +25,175 @@ void RendererGL::CreateWindowResources(WindowWrapper *windowWrapper)
 	SwapChainPanel^ swapChainPanel;
 	swapChainPanel = reinterpret_cast<SwapChainPanel^>(windowWrapper->swapChainPanel);
 
-	const EGLint configAttributes[] =
-	{
-		EGL_RED_SIZE, 8,
-		EGL_GREEN_SIZE, 8,
-		EGL_BLUE_SIZE, 8,
-		EGL_ALPHA_SIZE, 8,
-		EGL_DEPTH_SIZE, 8,
-		EGL_STENCIL_SIZE, 8,
-		EGL_NONE
-	};
+    const EGLint configAttributes[] =
+    {
+        EGL_RED_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_BLUE_SIZE, 8,
+        EGL_ALPHA_SIZE, 8,
+        EGL_DEPTH_SIZE, 8,
+        EGL_STENCIL_SIZE, 8,
+        EGL_NONE
+    };
 
-	const EGLint contextAttributes[] =
-	{
-		EGL_CONTEXT_CLIENT_VERSION, 2,
-		EGL_NONE
-	};
+    const EGLint contextAttributes[] =
+    {
+        EGL_CONTEXT_CLIENT_VERSION, 2,
+        EGL_NONE
+    };
 
-	const EGLint defaultDisplayAttributes[] =
-	{
-		// These are the default display attributes, used to request ANGLE's D3D11 renderer.
-		// eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
-		EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+    const EGLint surfaceAttributes[] =
+    {
+        // EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER is part of the same optimization as EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER (see above).
+        // If you have compilation issues with it then please update your Visual Studio templates.
+        EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE
+    };
 
-		// EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
-		// Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
-		EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+    const EGLint defaultDisplayAttributes[] =
+    {
+        // These are the default display attributes, used to request ANGLE's D3D11 renderer.
+        // eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
 
-		// EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE is an option that enables ANGLE to automatically call 
-		// the IDXGIDevice3::Trim method on behalf of the application when it gets suspended. 
-		// Calling IDXGIDevice3::Trim when an application is suspended is a Windows Store application certification requirement.
-		EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
-		EGL_NONE,
-	};
+        // EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
+        // Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE,
+    };
 
-	const EGLint fl9_3DisplayAttributes[] =
-	{
-		// These can be used to request ANGLE's D3D11 renderer, with D3D11 Feature Level 9_3.
-		// These attributes are used if the call to eglInitialize fails with the default display attributes.
-		EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
-		EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
-		EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
-		EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
-		EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
-		EGL_NONE,
-	};
+    const EGLint fl9_3DisplayAttributes[] =
+    {
+        // These can be used to request ANGLE's D3D11 renderer, with D3D11 Feature Level 9_3.
+        // These attributes are used if the call to eglInitialize fails with the default display attributes.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+        EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
+        EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE,
+    };
 
-	const EGLint warpDisplayAttributes[] =
-	{
-		// These attributes can be used to request D3D11 WARP.
-		// They are used if eglInitialize fails with both the default display attributes and the 9_3 display attributes.
-		EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
-		EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_DEVICE_TYPE_WARP_ANGLE,
-		EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
-		EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
-		EGL_NONE,
-	};
+    const EGLint warpDisplayAttributes[] =
+    {
+        // These attributes can be used to request D3D11 WARP.
+        // They are used if eglInitialize fails with both the default display attributes and the 9_3 display attributes.
+        EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+        EGL_PLATFORM_ANGLE_DEVICE_TYPE_HARDWARE_ANGLE, EGL_PLATFORM_ANGLE_DEVICE_TYPE_WARP_ANGLE,
+        EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+        EGL_NONE,
+    };
 
-	EGLConfig config = NULL;
+    EGLConfig config = NULL;
 
-	// eglGetPlatformDisplayEXT is an alternative to eglGetDisplay. It allows us to pass in display attributes, used to configure D3D11.
-	PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
-	if (!eglGetPlatformDisplayEXT)
-	{
-		throw Exception::CreateException(E_FAIL, L"Failed to get function eglGetPlatformDisplayEXT");
-	}
+    // eglGetPlatformDisplayEXT is an alternative to eglGetDisplay. It allows us to pass in display attributes, used to configure D3D11.
+    PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
+    if (!eglGetPlatformDisplayEXT)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to get function eglGetPlatformDisplayEXT");
+    }
 
-	//
-	// To initialize the display, we make three sets of calls to eglGetPlatformDisplayEXT and eglInitialize, with varying 
-	// parameters passed to eglGetPlatformDisplayEXT:
-	// 1) The first calls uses "defaultDisplayAttributes" as a parameter. This corresponds to D3D11 Feature Level 10_0+.
-	// 2) If eglInitialize fails for step 1 (e.g. because 10_0+ isn't supported by the default GPU), then we try again 
-	//    using "fl9_3DisplayAttributes". This corresponds to D3D11 Feature Level 9_3.
-	// 3) If eglInitialize fails for step 2 (e.g. because 9_3+ isn't supported by the default GPU), then we try again 
-	//    using "warpDisplayAttributes".  This corresponds to D3D11 Feature Level 11_0 on WARP, a D3D11 software rasterizer.
-	//
+    //
+    // To initialize the display, we make three sets of calls to eglGetPlatformDisplayEXT and eglInitialize, with varying 
+    // parameters passed to eglGetPlatformDisplayEXT:
+    // 1) The first calls uses "defaultDisplayAttributes" as a parameter. This corresponds to D3D11 Feature Level 10_0+.
+    // 2) If eglInitialize fails for step 1 (e.g. because 10_0+ isn't supported by the default GPU), then we try again 
+    //    using "fl9_3DisplayAttributes". This corresponds to D3D11 Feature Level 9_3.
+    // 3) If eglInitialize fails for step 2 (e.g. because 9_3+ isn't supported by the default GPU), then we try again 
+    //    using "warpDisplayAttributes".  This corresponds to D3D11 Feature Level 11_0 on WARP, a D3D11 software rasterizer.
+    //
+    // Note: On Windows Phone, we #ifdef out the first set of calls to eglPlatformDisplayEXT and eglInitialize.
+    //       Windows Phones devices only support D3D11 Feature Level 9_3, but the Windows Phone emulator supports 11_0+.
+    //       We use this #ifdef to limit the Phone emulator to Feature Level 9_3, making it behave more like
+    //       real Windows Phone devices.
+    //       If you wish to test Feature Level 10_0+ in the Windows Phone emulator then you should remove this #ifdef.
+    //
 
-	// This tries to initialize EGL to D3D11 Feature Level 10_0+. See above comment for details.
-	mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, defaultDisplayAttributes);
-	if (mEglDisplay == EGL_NO_DISPLAY)
-	{
-		throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
-	}
+#if (WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP)
+    // This tries to initialize EGL to D3D11 Feature Level 10_0+. See above comment for details.
+    mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, defaultDisplayAttributes);
+    if (mEglDisplay == EGL_NO_DISPLAY)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+    }
 
-	if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
-	{
-		// This tries to initialize EGL to D3D11 Feature Level 9_3, if 10_0+ is unavailable (e.g. on some mobile devices).
-		mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, fl9_3DisplayAttributes);
-		if (mEglDisplay == EGL_NO_DISPLAY)
-		{
-			throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
-		}
+    if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
+#endif    
+    {
+        // This tries to initialize EGL to D3D11 Feature Level 9_3, if 10_0+ is unavailable (e.g. on Windows Phone, or certain Windows tablets).
+        mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, fl9_3DisplayAttributes);
+        if (mEglDisplay == EGL_NO_DISPLAY)
+        {
+            throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+        }
 
-		if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
-		{
-			// This initializes EGL to D3D11 Feature Level 11_0 on WARP, if 9_3+ is unavailable on the default GPU.
-			mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, warpDisplayAttributes);
-			if (mEglDisplay == EGL_NO_DISPLAY)
-			{
-				throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
-			}
+        if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
+        {
+            // This initializes EGL to D3D11 Feature Level 11_0 on WARP, if 9_3+ is unavailable on the default GPU (e.g. on Surface RT).
+            mEglDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, EGL_DEFAULT_DISPLAY, warpDisplayAttributes);
+            if (mEglDisplay == EGL_NO_DISPLAY)
+            {
+                throw Exception::CreateException(E_FAIL, L"Failed to get EGL display");
+            }
 
-			if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
-			{
-				// If all of the calls to eglInitialize returned EGL_FALSE then an error has occurred.
-				throw Exception::CreateException(E_FAIL, L"Failed to initialize EGL");
-			}
-		}
-	}
+            if (eglInitialize(mEglDisplay, NULL, NULL) == EGL_FALSE)
+            {
+                // If all of the calls to eglInitialize returned EGL_FALSE then an error has occurred.
+                throw Exception::CreateException(E_FAIL, L"Failed to initialize EGL");
+            }
+        }
+    }
 
-	EGLint numConfigs = 0;
-	if ((eglChooseConfig(mEglDisplay, configAttributes, &config, 1, &numConfigs) == EGL_FALSE) || (numConfigs == 0))
-	{
-		throw Exception::CreateException(E_FAIL, L"Failed to choose first EGLConfig");
-	}
+    EGLint numConfigs = 0;
+    if ((eglChooseConfig(mEglDisplay, configAttributes, &config, 1, &numConfigs) == EGL_FALSE) || (numConfigs == 0))
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to choose first EGLConfig");
+    }
 
-	mEglContext = eglCreateContext(mEglDisplay, config, EGL_NO_CONTEXT, contextAttributes);
-	if (mEglContext == EGL_NO_CONTEXT)
-	{
-		throw Exception::CreateException(E_FAIL, L"Failed to create EGL context");
-	}
-
-	const EGLint surfaceAttributes[] =
-	{
-		// EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER is part of the same optimization as EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER (see above).
-		// If you have compilation issues with it then please update your Visual Studio templates.
-		EGL_ANGLE_SURFACE_RENDER_TO_BACK_BUFFER, EGL_TRUE,
-		EGL_NONE
-	};
-
-	// Create a PropertySet and initialize with the EGLNativeWindowType.
-	PropertySet^ surfaceCreationProperties = ref new PropertySet();
+    // Create a PropertySet and initialize with the EGLNativeWindowType.
+    PropertySet^ surfaceCreationProperties = ref new PropertySet();
 
 	if (swapChainPanel)
 	{
 		surfaceCreationProperties->Insert(ref new String(EGLNativeWindowTypeProperty), swapChainPanel);
 	}
-
-	mEglSurface = eglCreateWindowSurface(mEglDisplay, config, reinterpret_cast<IInspectable*>(surfaceCreationProperties), surfaceAttributes);
-	if (mEglSurface == EGL_NO_SURFACE)
+	else
 	{
-		throw Exception::CreateException(E_FAIL, L"Failed to create EGL surface");
+		// TODO: CoreWindow
+		assert(false);
 	}
 
-	if (eglMakeCurrent(mEglDisplay, mEglSurface, mEglSurface, mEglContext) == EGL_FALSE)
-	{
-		throw Exception::CreateException(E_FAIL, L"Failed to make EGLSurface current");
-	}
+    //
+    // A Custom render surface size can be specified by uncommenting the following lines.
+    // The render surface will be automatically scaled to fit the entire window.  Using a
+    // smaller sized render surface can result in a performance gain.
+    //
+    //mCustomRenderSurfaceSize = Size(800, 600);
+    //mUseCustomRenderSurfaceSize = true;
+    //surfaceCreationProperties->Insert(ref new String(EGLRenderSurfaceSizeProperty), PropertyValue::CreateSize(mCustomRenderSurfaceSize));
+    //
+
+    mEglSurface = eglCreateWindowSurface(mEglDisplay, config, reinterpret_cast<IInspectable*>(surfaceCreationProperties), surfaceAttributes);
+    if (mEglSurface == EGL_NO_SURFACE)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to create EGL fullscreen surface");
+    }
+
+    mEglContext = eglCreateContext(mEglDisplay, config, EGL_NO_CONTEXT, contextAttributes);
+    if (mEglContext == EGL_NO_CONTEXT)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to create EGL context");
+    }
+
+    if (eglMakeCurrent(mEglDisplay, mEglSurface, mEglSurface, mEglContext) == EGL_FALSE)
+    {
+        throw Exception::CreateException(E_FAIL, L"Failed to make fullscreen EGLSurface current");
+    }
+
+	EGLint width; EGLint height;
+	eglQuerySurface(mEglDisplay, mEglSurface, EGL_WIDTH, &width);
+	eglQuerySurface(mEglDisplay, mEglSurface, EGL_HEIGHT, &height);
+
+	glViewport(0, 0, width, height);
 }
 
 void RendererGL::DestroyWindowResources()
