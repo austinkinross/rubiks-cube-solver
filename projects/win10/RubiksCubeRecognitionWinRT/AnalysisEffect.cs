44810fe3cfc84c070783eedb7c35b4a7058744fd
Austin Kinross <austinkinross@outlook.com>
2015-07-17 09:53:38 -0700

    AnalysisEffect detects locations and colors of cubelets

--- projects/win10/RubiksCubeRecognitionWinRT/AnalysisEffect.cs
+++ projects/win10/RubiksCubeRecognitionWinRT/AnalysisEffect.cs
@@ -141,75 +141,87 @@ namespace RubiksCubeRecognitionWinRT
             int centerY = 0;
             int cubletWidth = 0;
             int cubletHeight = 0;
+                
+            int analysisAreaX = (int)rt.SizeInPixels.Width * 3 / 10;
+            int analysisWidth = (int)rt.SizeInPixels.Width * 4 / 10;
+            byte[] analysisHorzBytes = rt.GetPixelBytes(analysisAreaX, (int)rt.SizeInPixels.Height / 2, analysisWidth, 1);
+
+            int analysisAreaY = (int)rt.SizeInPixels.Height * 3 / 10;
+            int analysisHeight = (int)rt.SizeInPixels.Height * 4 / 10;
+            byte[] analysisVertBytes = rt.GetPixelBytes((int)rt.SizeInPixels.Width / 2, analysisAreaY, 1, analysisHeight);
+
+            int foundLeft = 0;
+            for (int i = 0; i < analysisWidth / 2; i++)
+            {
+                byte b = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 0];
+                byte g = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 1];
+                byte r = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 2];
+                if ((r > 100 ||  g > 100 || b > 80) && foundLeft == 0)
+                {
+                    foundLeft = i;
+                }
+            }
+
+            int foundRight = 0;
+            for (int i = 0; i < analysisWidth / 2; i++)
+            {
+                byte r = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 0];
+                byte g = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 1];
+                byte b = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 2];
+                if ((r > 100 || g > 100 || b > 80) && foundRight == 0)
+                {
+                    foundRight = i;
+                }
+            }
+
+            int foundTop = 0;
+            for (int i = 0; i < analysisHeight / 2; i++)
+            {
+                byte r = analysisVertBytes[4 * (analysisHeight / 2 - i) + 0];
+                byte g = analysisVertBytes[4 * (analysisHeight / 2 - i) + 1];
+                byte b = analysisVertBytes[4 * (analysisHeight / 2 - i) + 2];
+                if ((r > 100 || g > 100 || b > 80) && foundTop == 0)
+                {
+                    foundTop = i;
+                }
+            }
+
+            int foundBottom = 0;
+            for (int i = 0; i < analysisHeight / 2; i++)
+            {
+                byte r = analysisVertBytes[4 * (analysisHeight / 2 + i) + 0];
+                byte g = analysisVertBytes[4 * (analysisHeight / 2 + i) + 1];
+                byte b = analysisVertBytes[4 * (analysisHeight / 2 + i) + 2];
+                if ((r > 100 || g > 100 || b > 80) && foundBottom == 0)
+                {
+                    foundBottom = i;
+                }
+            }
+
+            centerX = (-foundLeft + foundRight) / 2 + (int)rt.SizeInPixels.Width / 2;
+            centerY = (-foundTop + foundBottom) / 2 + (int)rt.SizeInPixels.Height / 2;
+            cubletWidth = (int)((foundLeft + foundRight) * 1.2f);
+            cubletHeight = (int)((foundTop + foundBottom) * 1.2f);
+
+            // No 2d arrays in WinRT components? Boo.
+            // "Error C1113	#using failed on 'rubikscuberecognitionwinrt.winmd'	RubiksCubeRecognitionLib
+            Vector2[] cubletCenters = new Vector2[3 * 3];
+            Color[] cubletColors = new Color[3 * 3];
+
+            cubletCenters[1 * 3 + 1] = new Vector2(centerX, centerY);
+            cubletCenters[1 * 3 + 0] = new Vector2(centerX, centerY - cubletHeight);
+            cubletCenters[1 * 3 + 2] = new Vector2(centerX, centerY + cubletHeight);
+            cubletCenters[0 * 3 + 1] = new Vector2(centerX - cubletWidth, centerY);
+            cubletCenters[0 * 3 + 0] = new Vector2(centerX - cubletWidth, centerY - cubletHeight);
+            cubletCenters[0 * 3 + 2] = new Vector2(centerX - cubletWidth, centerY + cubletHeight);
+            cubletCenters[2 * 3 + 1] = new Vector2(centerX + cubletWidth, centerY);
+            cubletCenters[2 * 3 + 0] = new Vector2(centerX + cubletWidth, centerY - cubletHeight);
+            cubletCenters[2 * 3 + 2] = new Vector2(centerX + cubletWidth, centerY + cubletHeight);
 
             using (CanvasBitmap input = CanvasBitmap.CreateFromDirect3D11Surface(canvasDevice, context.InputFrame.Direct3DSurface))
             using (CanvasRenderTarget output = CanvasRenderTarget.CreateFromDirect3D11Surface(canvasDevice, context.OutputFrame.Direct3DSurface))
             using (CanvasDrawingSession ds = output.CreateDrawingSession())
             {
-                Debug.WriteLine(output.Format.ToString());
-                
-                int analysisAreaX = (int)rt.SizeInPixels.Width * 3 / 10;
-                int analysisWidth = (int)rt.SizeInPixels.Width * 4 / 10;
-                byte[] analysisHorzBytes = rt.GetPixelBytes(analysisAreaX, (int)output.SizeInPixels.Height / 2, analysisWidth, 1);
-
-                int analysisAreaY = (int)rt.SizeInPixels.Height * 3 / 10;
-                int analysisHeight = (int)rt.SizeInPixels.Height * 4 / 10;
-                byte[] analysisVertBytes = rt.GetPixelBytes((int)output.SizeInPixels.Width / 2, analysisAreaY, 1, analysisHeight);
-
-                int foundLeft = 0;
-                for (int i = 0; i < analysisWidth / 2; i++)
-                {
-                    byte b = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 0];
-                    byte g = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 1];
-                    byte r = analysisHorzBytes[4 * (analysisWidth / 2 - i) + 2];
-                    if ((r > 100 ||  g > 100 || b > 80) && foundLeft == 0)
-                    {
-                        foundLeft = i;
-                    }
-                }
-
-                int foundRight = 0;
-                for (int i = 0; i < analysisWidth / 2; i++)
-                {
-                    byte r = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 0];
-                    byte g = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 1];
-                    byte b = analysisHorzBytes[4 * (analysisWidth / 2 + i) + 2];
-                    if ((r > 100 || g > 100 || b > 80) && foundRight == 0)
-                    {
-                        foundRight = i;
-                    }
-                }
-
-                int foundTop = 0;
-                for (int i = 0; i < analysisHeight / 2; i++)
-                {
-                    byte r = analysisVertBytes[4 * (analysisHeight / 2 - i) + 0];
-                    byte g = analysisVertBytes[4 * (analysisHeight / 2 - i) + 1];
-                    byte b = analysisVertBytes[4 * (analysisHeight / 2 - i) + 2];
-                    if ((r > 100 || g > 100 || b > 80) && foundTop == 0)
-                    {
-                        foundTop = i;
-                    }
-                }
-
-                int foundBottom = 0;
-                for (int i = 0; i < analysisHeight / 2; i++)
-                {
-                    byte r = analysisVertBytes[4 * (analysisHeight / 2 + i) + 0];
-                    byte g = analysisVertBytes[4 * (analysisHeight / 2 + i) + 1];
-                    byte b = analysisVertBytes[4 * (analysisHeight / 2 + i) + 2];
-                    if ((r > 100 || g > 100 || b > 80) && foundBottom == 0)
-                    {
-                        foundBottom = i;
-                    }
-                }
-
-                centerX = (-foundLeft + foundRight) / 2 + (int)output.SizeInPixels.Width / 2;
-                centerY = (-foundTop + foundBottom) / 2 + (int)output.SizeInPixels.Height / 2;
-                cubletWidth = (int)((foundLeft + foundRight) * 1.2f);
-                cubletHeight = (int)((foundTop + foundBottom) * 1.2f);
-
-
                 var transformEffect = new Transform2DEffect()
                 {
                     Source = input,
@@ -219,18 +231,40 @@ namespace RubiksCubeRecognitionWinRT
                 Rect src = new Rect(0, 0, output.SizeInPixels.Width, output.SizeInPixels.Height);
                 ds.DrawImage(transformEffect, src, src);
 
+                // Draw a crosshair on the screen
                 ds.FillRectangle(new Rect(output.SizeInPixels.Width / 2 - 3, output.SizeInPixels.Height / 4, 6, output.SizeInPixels.Height / 2), Colors.Gray);
                 ds.FillRectangle(new Rect(output.SizeInPixels.Width / 4, output.SizeInPixels.Height / 2 - 3, output.SizeInPixels.Width / 2, 6), Colors.Gray);
 
-                ds.FillRectangle(centerX - 10, centerY - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX - 10, centerY - cubletHeight - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX - 10, centerY + cubletHeight - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX - cubletWidth - 10, centerY - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX - cubletWidth - 10, centerY - cubletHeight - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX - cubletWidth - 10, centerY + cubletHeight - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX + cubletWidth - 10, centerY - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX + cubletWidth - 10, centerY - cubletHeight - 10, 20, 20, Colors.Black);
-                ds.FillRectangle(centerX + cubletWidth - 10, centerY + cubletHeight - 10, 20, 20, Colors.Black);
+                if (true)
+                {
+                    for (int x = 0; x < 3; x++)
+                    {
+                        for (int y = 0; y < 3; y++)
+                        {
+                            int sampleWidth = 2;
+
+                            byte[] cubletBytes = input.GetPixelBytes((int)cubletCenters[(2 - x) * 3 + y].X - sampleWidth / 2, (int)cubletCenters[(2 - x) * 3 + y].Y - sampleWidth / 2, sampleWidth, sampleWidth);
+                            int totalR = 0; int totalG = 0; int totalB = 0;
+                            for (int i = 0; i < sampleWidth * sampleWidth; i++)
+                            {
+                                totalB += cubletBytes[4 * i + 0];
+                                totalG += cubletBytes[4 * i + 1];
+                                totalR += cubletBytes[4 * i + 2];
+                            }
+
+                            cubletColors[x * 3 + y] = Color.FromArgb(255, (byte)(totalR / (sampleWidth * sampleWidth)), (byte)(totalG / (sampleWidth * sampleWidth)), (byte)(totalB / (sampleWidth * sampleWidth)));
+                        }
+                    }
+
+                    for (int x = 0; x < 3; x++)
+                    {
+                        for (int y = 0; y < 3; y++)
+                        {
+                            ds.FillRectangle((float)cubletCenters[x * 3 + y].X - 12, (float)cubletCenters[x * 3 + y].Y - 12, 24, 24, Colors.Black);
+                            ds.FillRectangle((float)cubletCenters[x * 3 + y].X - 10, (float)cubletCenters[x * 3 + y].Y - 10, 20, 20, cubletColors[x * 3 + y]);
+                        }
+                    }
+                }
             }
         }
 
